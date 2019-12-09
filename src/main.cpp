0b05c793833302ea96d54421320364321b196d20
Kostas Chatzikokolakis <kostas@chatzi.org>
2019-12-09 10:05:29 +0200

    initializer list, find(M < k)

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,12 @@ using arma::Mat;
 using arma::Row;
 
 void integer() {
-	Mat<int_t> M(2,2);
+	Mat<int_t> M {
+		{ int_t(1), int_t(4) },
+		{ int_t(2), int_t(5) },
+	};
 
-	cout << "M(2,2)\n" << M;
+	cout << "From initializer list\n" << M;
 	cout << "M.ones()\n" << M.ones();
 	cout << "M = 'string'\n" << (M  = "1 3; -1 -6");
 	cout << "M:\n" << M;
@@ -27,12 +30,16 @@ void integer() {
 	cout << "M * -3\n" << (M * int_t(-3));
 	cout << "M.each_row() + Row(2,3)\n" << (M.each_row() + Row<int_t>("2 3"));
 	cout << "M == -3\n" << (M == int_t(-3)) << "\n";
+	cout << "find(M < 2): " << arma::find(M < int_t(2)) << "\n";
 }
 
 void rational() {
-	Mat<rat_t> M(2,2);
+	Mat<rat_t> M = {
+		{ rat_t(1,2), rat_t(4,3) },
+		{ rat_t(2,7), rat_t(5,3) },
+	};
 
-	cout << "M(2,2)\n" << M;
+	cout << "From initializer list\n" << M;
 	cout << "M.ones()\n" << M.ones();
 	cout << "M.randu()\n" << M.randu();
 	cout << "M = 'string'\n" << (M  = "1/2 3/1; -1/3 -6");
@@ -45,6 +52,7 @@ void rational() {
 	cout << "M * -1/3\n" << (M * rat_t(-1,3));
 	cout << "M.each_row() + Row(1/2,1/3)\n" << (M.each_row() + Row<rat_t>("1/2 1/3"));
 	cout << "M == -1/3\n" << (M == rat_t(-1,3)) << "\n";
+	cout << "find(M < 1/2): " << arma::find(M < rat_t(1,2)) << "\n";
 }
 
 int main() {
